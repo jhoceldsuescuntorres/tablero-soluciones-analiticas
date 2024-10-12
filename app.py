59af316cc018ba764ba2ded2604c1af402661178
Jhocel Suescun <jhocelduvanst@gmail.com>
2024-10-12 09:29:35 -0500

    Modificar funcion para lectura de data para el dashboard

--- app.py
+++ app.py
@@ -21,7 +21,13 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    dataframe = pd.read_csv('datos_energia.csv')
+    # Establecer la columna en datetime
+    dataframe['time'] = pd.to_datetime(dataframe['time'])
+
+    # Establecer  la columna de fecha como índice del DataFrame
+    dataframe.set_index('time', inplace=True)
+    return dataframe
     
 
 # Cargar datos
